9dfc059896d0c161ddf136127dcbea0c32e7738c
osj <tmdwn8775@gmail.com>
2023-11-13 02:38:34 +0900

    Add: login 폴더 구조 변경,  login 페이지 구현

---
+++ src/login/App.js
@@ -0,0 +1,52 @@
+import logo from '../logo.svg';
+
+import './App.css';
+
+import React, { Component } from "react";
+
+import { getResizeEventListener } from "../services/responsiveFrame";
+
+class App extends Component {
+  render() {
+    return (
+      <div id="App">
+        
+          <div className="e14_33"></div><a href = "회원가입 페이지" className="e14_57">회원가입</a>
+
+          {/* <div className="e14_40">
+
+            <div className="e14_41"></div><span className="e14_42">로그인</span>
+
+          </div> */}
+
+          <div className="e14_34">
+
+            <div className="e14_35"></div>{/* <span className="e14_36">비밀번호</span> */}
+
+          </div>
+
+          <div className="e14_37">
+
+            <div className="e14_38"></div>{/* <span className="e14_39">아이디</span> */}
+
+          </div>
+          <span className="e14_43">카카오톡 추억 저장소</span>
+
+        <div className="e14_44"></div>
+        
+        <form method="post" action="서버의url" id="login-form">
+          <input type="text" name="userName" placeholder="아이디"></input>
+          <input type="password" name="userPassword" placeholder="비밀번호"></input>
+          <input type="submit" value="로그인"></input>
+        </form>
+      </div>
+    );
+  }
+  componentDidMount() {
+    const FixRatio = getResizeEventListener(1920, 1080);
+    window.onresize = FixRatio;
+    FixRatio();
+  }
+}
+
+export default App;
