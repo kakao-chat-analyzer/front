4204d119cdfb22c1d974d494876532b260560e6a
osj <tmdwn8775@gmail.com>
2023-11-21 16:38:10 +0900

    Add: 스크롤 성공!

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,59 +1,25 @@
-import React from "react";
+import React, { useState, useRef } from 'react';
 // import { getResizeEventListener } from "../services/responsiveFrame";
 import '../styles/detail.css';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-/* import { LeftArrow, RightArrow } from './Arrow'; */
-import { useState, useEffect } from 'react';
 
-const Arrow = ({ disabled, onClick, children }) => (
-   <button onClick={onClick} disabled={disabled}>
-      {children}
-   </button>
-);
+/* import { LeftArrow, RightArrow } from './Arrow'; */
+
+
+
+
 
 
 const Detail = () => {
 
-   const [selected, setSelected] = useState([]); // State to manage selected items
+    const [isScroll, setIsScroll] = useState(false);
+    const [originX, setOriginX] = useState(0);
+    const [afterX, setAfterX] = useState(0);
+    const [position, setPosition] = useState(0);
 
-   // Handle click event on each item
-   const handleClick = (id) => ({ getItemById, scrollToItem }) => {
-      const itemSelected = isItemSelected(id);
-
-      setSelected((currentSelected) =>
-         itemSelected
-            ? currentSelected.filter((el) => el !== id)
-            : currentSelected.concat(id)
-      );
-   };
-
-   // Function to check if an item is selected
-   const isItemSelected = (id) => selected.includes(id);
-
-   // Function to render the left arrow
-   const LeftArrow = () => {
-      const { isFirstItemVisible, scrollPrev } = React.useContext(VisibilityContext);
-
-      return (
-         <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
-            Left
-         </Arrow>
-      );
-   };
-
-   // Function to render the right arrow
-   const RightArrow = () => {
-      const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
-
-      return (
-         <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
-            Right
-         </Arrow>
-      );
-   };
-
-
-
+   /* const [isScrolling, setIsScrolling] = useState(false);
+   const [startX, setStartX] = useState(0);
+   const [scrollLeft, setScrollLeft] = useState(0);
+   const containerRef = useRef(null); */
 
    const Data = [
       { class: "e271_2" },
@@ -62,32 +28,101 @@ const Detail = () => {
       { class: "e271_4" },
       { class: "e271_5" },
       { class: "e271_6" },
-      // ... 추가 데이터
+      // ... Additional data
    ];
 
-   return (
-      <div id="App">
-         <div class="e272_2">
-            <div class="e272_3"></div>
+   const handleScroll = (e, scroll) => {
+      switch (scroll) {
+         case 'start':
+            setOriginX(e.clientX);
+            setIsScroll(true);
+            break;
+         case 'end':
+            setAfterX(position);
+            setIsScroll(false);
+            break;
+         case 'leave':
+            setIsScroll(false);
+            break;
+         default:
+            break;
+      }
+   };
 
-            <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-               {Data.map(({ class: className }, index) => (
-                  <div key={index} className={className}></div>
-               ))}
-            </ScrollMenu>
+   const handleSlide = (e) => {
+      const scrollSpeed = 1.55; // Adjust this value to change the scroll speed
+      const newPosition = -(e.clientX - originX) * scrollSpeed + afterX;
+      const hiddenLength = e.currentTarget.offsetWidth - 166 * Data.length;
+
+      if (!isScroll) {
+         return;
+      }
+
+      if (newPosition > 10 && newPosition < hiddenLength) {
+         setPosition(newPosition);
+      }
+   };
+
+   /* const handleMouseDown = (e) => {
+      setIsScrolling(true);
+      setStartX(e.pageX - containerRef.current.offsetLeft);
+      setScrollLeft(containerRef.current.scrollLeft);
+
+   };
+
+   const handleMouseUp = () => {
+      setIsScrolling(false);
+   };
+
+   const handleMouseMove = (e) => {
+      if (!isScrolling) return;
+      e.preventDefault();
+      const x = e.pageX - containerRef.current.offsetLeft;
+      const distance = x - startX;
+      
+      containerRef.current.scrollLeft = scrollLeft - distance; // Change '+' to '-' to move in the opposite direction of the mouse
+      
+   }; */
+
+   return (
+      <div id="App" >
+
+         <div className="e272_2" >
+
+            <div class="e272_3"></div>
 
             <div class="e272_39"></div>
             <div class="e272_4">
                <div class="e272_5"></div>
             </div>
-            <span className="e42_10">카카오톡</span>
-            <span className="e42_11">추억 저장소</span>
-            <span className="e42_12">오승주님</span>
-            <span className="e204_6">이용방법</span>
-            <div className="e54_2"></div>
-            <div class="e272_48"></div>
+            <div
+
+               onMouseDown={(e) => handleScroll(e, 'start')}
+               onMouseUp={(e) => handleScroll(e, 'end')}
+               onMouseLeave={(e) => handleScroll(e, 'leave')}
+               onMouseMove={handleSlide}
+               style={{
+                  transform: `translateX(-${position}px)`,
+                  cursor: isScroll ? 'grabbing' : 'grab',
+                  userSelect: 'none',
+               }}
+            >
+               {Data.map(({ class: className }, index) => (
+                  <div key={index} className={className} ></div>
+               ))}</div>
+               <span className="e42_10">카카오톡</span>
+               <span className="e42_11">추억 저장소</span>
+               <span className="e42_12">오승주님</span>
+               <span className="e204_6">이용방법</span>
+               <div className="e54_2"></div>
+               <div class="e272_48"></div>
+
+            
+
+
          </div>
       </div>
+
    );
 };
 
